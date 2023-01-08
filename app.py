476bf9bc921b3e674f3ccf2f80ca503c709432fb
Oscar Chamat <chamatoscar@gmail.com>
2023-01-08 15:01:28 -0500

    error with number-destination.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def msm():
     request_data = request.get_json()
     msm = request_data['msm']
     number_origin = request_data['number-origin']
-    number_destination = request_data['number-destination']
+    number_destination = request_data.get('number-destination', None)
     print(number_origin, "=>", number_destination)
     print("-" * 100)
     print(msm)
